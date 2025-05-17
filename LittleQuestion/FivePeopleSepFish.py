ce46b0d3f560cedd6d6b1be2b1d57843b6d2d137
BaimaAmbrose <110985729+BaimaAmbrose@users.noreply.github.com>
2025-05-17 23:51:54 +0900

    Initial commit

---
+++ LittleQuestion/FivePeopleSepFish.py
@@ -0,0 +1,15 @@
+fish = 6
+while True:
+    total = fish
+    enough = True
+    for _ in range(5):
+        if (total - 1) % 5 == 0:
+            total = (total - 1) // 5 * 4
+        else:
+            enough = False
+            break
+    if enough:
+        break
+    else:
+        fish += 5
+print(fish)
